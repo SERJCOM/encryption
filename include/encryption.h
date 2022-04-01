c51665c831f5683f36b90d2228be224259e1b115
SERJCOM <sergeylukin228@yandex.ru>
2022-04-01 23:55:12 +0300

    ладно

--- include/encryption.h
+++ include/encryption.h
@@ -1,18 +1,33 @@
 #pragma once
 
 #include <string>
-#include "uint256_t.h"  
+#include "uint256_t.h" 
+
 
 class Encryption{
+public:
+
+    void SetPrivateKey(uint256_t private_key);
+
+    bool Verify(uint256_t public_key, uint256_t signature, std::string text);
+
+    uint256_t GetPrivateKey();
+
+	uint256_t GenerateSignature(uint256_t private_key, std::string text);
+    
+    Encryption(const std::string p_string, const std::string x_string, const std::string y_string, const std::string n_string);
+
 private:
     uint256_t private_key = 0;
     uint256_t public_key = 0;
-    uint256_t p = 0;
+    uint256_t p;
+    uint256_t n;
+    uint256_t message_hash;
     //base point
     uint256_t x = 0;
     uint256_t y = 0;
-    uint256_t n;
-    uint256_t message_hash;
-public:
-    uint256_t get_private_key(){return this->private_key;}
+    
+    uint256_t s(uint256_t x1,uint256_t y1,uint256_t x2,uint256_t y2);
+
+    
 };
--- src/encryption.cpp
+++ src/encryption.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
+#include "encryption.h"
+#include "uint256_t.h" 
+#include <string>
 
+Encryption::Encryption(const std::string p_string, const std::string x_string, const std::string y_string, const std::string n_string){
+    uint8_t base = 10;
+    this->p = uint256_t(p_string, base);
+    this->x = uint256_t(x_string, base);
+    this->y = uint256_t(y_string, base);
+    this->n = uint256_t(n_string, base);
+}
