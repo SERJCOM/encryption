b3d92b7912bcc5e7a1d87bd7016ee6e05c0a842f
SERJCOM <sergeylukin228@yandex.ru>
2022-04-02 02:50:48 +0300

    все функции доделаны

--- include/encryption.h
+++ include/encryption.h
@@ -7,9 +7,19 @@
 class Encryption{
 public:
 
-    void SetPrivateKey(uint256_t private_key);
+    struct sigMessage{
+        uint256_t r, message;
+    };
 
-    bool Verify(uint256_t public_key, uint256_t signature, std::string text);
+    struct PublicKey{
+        uint256_t x,y;
+    };
+
+    PublicKey CreatePublicKey();
+
+    void SetPrivateKey(std::string private_key);
+
+    bool Verify(PublicKey public_key, Encryption::sigMessage signature, uint256_t text);
 
     uint256_t GetPrivateKey();
 
@@ -17,29 +27,31 @@ public:
 
     Encryption(const std::string p_string, const std::string x_string, const std::string y_string, const std::string n_string);
 
-    uint256_t sign(uint256_t message, uint256_t x, uint256_t y, uint256_t p, uint256_t private_key);
+    sigMessage Sign(uint256_t message);
 
     uint256_t gcd(uint256_t num1, uint256_t num2, uint256_t &x, uint256_t &y); // advanced euclid algorithm
 
-private:
-    uint256_t private_key = 0;
-    uint256_t public_key = 0;
     uint256_t p;
+
+private:
+    uint256_t private_key;
+    uint256_t public_key ;
+    
     uint256_t n;
     uint256_t message_hash;
     //base point
     uint256_t x = 0;
     uint256_t y = 0;
     
-    uint256_t s(uint256_t x1,uint256_t y1,uint256_t x2,uint256_t y2, uint256_t p);
+    uint256_t s(uint256_t x1,uint256_t y1,uint256_t x2,uint256_t y2);
 
-    uint256_t xNew(uint256_t x1, uint256_t x2, uint256_t result_s, uint256_t p);
+    uint256_t xNew(uint256_t x1, uint256_t x2, uint256_t result_s);
 
-    uint256_t yNew(uint256_t x1, uint256_t x3, uint256_t y1 , uint256_t result_s, uint256_t p);
+    uint256_t yNew(uint256_t x1, uint256_t x3, uint256_t y1 , uint256_t result_s);
 
     
 
-    void point_multiplication(uint256_t multiplier, uint256_t &x, uint256_t &y, uint256_t p);
+    void point_multiplication(uint256_t multiplier, uint256_t &x, uint256_t &y);
 
     
 };
--- src/encryption.cpp
+++ src/encryption.cpp
@@ -3,10 +3,11 @@
 #include "uint256_t.h" 
 #include <string>
 #include <vector>
+#include <ctime>
 
 // TO DO
 // СДЕЛАТЬ РЕАЛИЗАЦИЮ ФУНКЦИИ S, SetPrivateKey, Verify, GetPrivateKey, GenerateSignature, 
-// 
+// ПОМЕНЯТЬ ПЕРЕМЕННУЮ n НА КОНСТАНТУ 
 
 Encryption::Encryption(const std::string p_string, const std::string x_string, const std::string y_string, const std::string n_string){
     uint8_t base = 10;
@@ -14,6 +15,16 @@ Encryption::Encryption(const std::string p_string, const std::string x_string, c
     this->x = uint256_t(x_string, base);
     this->y = uint256_t(y_string, base);
     this->n = uint256_t(n_string, base);
+}
+
+void Encryption::SetPrivateKey(const std::string privatekey){
+    
+    uint8_t base = 10;
+    this->private_key = uint256_t(privatekey, base);
+}
+
+uint256_t Encryption::GetPrivateKey(){
+    return this->private_key;
 }
 
 uint256_t Encryption::gcd(uint256_t num1, uint256_t num2, uint256_t &x, uint256_t &y){
@@ -28,28 +39,28 @@ uint256_t Encryption::gcd(uint256_t num1, uint256_t num2, uint256_t &x, uint256_
 	return d;
 }
 
-uint256_t Encryption::s(uint256_t x1,uint256_t y1,uint256_t x2,uint256_t y2, uint256_t p){
+uint256_t Encryption::s(uint256_t x1,uint256_t y1,uint256_t x2,uint256_t y2){
     if(x1 == x2 && y1 == y2){
         uint256_t y,x;
         uint256_t ynew = gcd(p, 2 * y2, x,y);
-        return (3 * x1 * x1 * y) % p;
+        return (3 * x1 * x1 * y) % this->p;
     }
     else{
         uint256_t y,x;
         uint256_t ynew = gcd(p, x2 - x1, x,y);
-        return ((y2 - y1) * y) % p;
+        return ((y2 - y1) * y) % this->p;
     }
 }
 
-uint256_t Encryption::xNew(uint256_t x1, uint256_t x2, uint256_t result_s, uint256_t p){
-    return (result_s * result_s - x1 - x2) % p;
+uint256_t Encryption::xNew(uint256_t x1, uint256_t x2, uint256_t result_s){
+    return (result_s * result_s - x1 - x2) % this->p;
 }
 
-uint256_t Encryption::yNew(uint256_t x1, uint256_t x3, uint256_t y1 , uint256_t result_s, uint256_t p){
-    return (result_s * (x1 - x3) - y1) % p;
+uint256_t Encryption::yNew(uint256_t x1, uint256_t x3, uint256_t y1 , uint256_t result_s){
+    return (result_s * (x1 - x3) - y1) % this->p;
 }
 
-void Encryption::point_multiplication(uint256_t multiplier, uint256_t &x, uint256_t &y, uint256_t p){
+void Encryption::point_multiplication(uint256_t multiplier, uint256_t &x, uint256_t &y){
 
     struct point{
         uint256_t x, y;
@@ -71,18 +82,18 @@ void Encryption::point_multiplication(uint256_t multiplier, uint256_t &x, uint25
         
         if(new_multiplier * 2 <= multiplier){
             new_multiplier = new_multiplier * 2;
-            uint256_t result = s(xa,ya,xa,ya,p);
-            uint256_t xa1 = xNew(xa,xa,result,p);
-            ya = yNew(xa,xa1, ya, result, p);
+            uint256_t result = s(xa,ya,xa,ya);
+            uint256_t xa1 = xNew(xa,xa,result);
+            ya = yNew(xa,xa1, ya, result);
             xa = xa1;
         }
         else{
-            for(int i = a.size() - 1; i > -1; i--){
+            for(size_t i = a.size() - 1; i > -1; i--){
                 if(new_multiplier + a[i] <= multiplier){
                     new_multiplier = new_multiplier + a[i];
-                    uint256_t result = s(xa,ya, b[i].x, b[i].y, p);
-                    uint256_t xa1 = xNew(xa,b[i].x, result, p);
-                    ya = yNew(xa,xa1, ya, result, p);
+                    uint256_t result = s(xa,ya, b[i].x, b[i].y);
+                    uint256_t xa1 = xNew(xa,b[i].x, result);
+                    ya = yNew(xa,xa1, ya, result);
                     xa = xa1;
                 }
             }
@@ -91,3 +102,62 @@ void Encryption::point_multiplication(uint256_t multiplier, uint256_t &x, uint25
     }
 }
 
+uint256_t getRandomNumber(uint256_t min, uint256_t max)
+{
+    static const uint256_t fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+    return static_cast<uint256_t>(rand() * fraction * (max - min + 1) + min);
+}
+
+Encryption::sigMessage Encryption::Sign(std::string message_text){
+    uint256_t message = uint256_t(message_text, 10);
+    uint256_t r = 0;
+    sigMessage sig;
+    uint256_t k = 0;
+    while(k == 0 || r == 0 || message == 0){
+        //k = getRandomNumber(1, this->n - 1);
+        k = 9;
+        uint256_t x = this->x, y = this->y, xgcd, ygcd;
+        point_multiplication(k, x,y);
+        r = x % n;
+        uint256_t gcdnew = gcd(this->n, k, xgcd,ygcd);
+        message = ((message + r * this->private_key) * ygcd) % this->n;
+    }
+
+    sig.message = message;
+    sig.r = r;
+
+    return sig;
+}
+
+Encryption::PublicKey Encryption::CreatePublicKey(){
+    PublicKey pr;
+    point_multiplication(this->private_key, pr.x, pr.y);
+    return pr;
+}
+
+bool Encryption::Verify(Encryption::PublicKey public_key, Encryption::sigMessage signature, uint256_t text){
+    bool flag = false;
+    if(signature.r >= 1 && signature.r < this->n - 1 && signature.message >= 1 && signature.message < this->n - 1){
+        uint256_t xgcd, ygcd, w, u, v, xuG, yuG, xvQ = public_key.x, yvQ = public_key.y;
+        w = gcd(n, signature.message, xgcd, ygcd);
+        w = ygcd;
+
+        u = (text * w) % this->n;
+
+        v = (signature.r * w) % this->n;
+
+        point_multiplication(u,  xuG, yuG);
+        point_multiplication(v, xvQ , yvQ);
+
+        uint256_t result = (xuG, yuG, xvQ, yvQ);
+
+        uint256_t xv, yv;
+        xv = xNew(xuG, xvQ, result);
+        yv = yNew(xuG, xv, yuG, result);
+
+        if(signature.r == xv % this->n){
+            flag = true;
+        }
+    }
+    return flag;
+}
